86f58698d14b0db55096b12817e02dbf1c3739e8
Ivan Timokhov <timokhovim@gmail.com>
2022-12-08 15:35:26 +0400

    BugFix
    Reference to Client obj

--- client.go
+++ client.go
@@ -24,28 +24,28 @@ func NewClient(url string, apiVersion string) *Client {
 	return &Client{url: url, apiVersion: apiVersion}
 }
 
-func (c Client) Url(url ...string) string {
+func (c *Client) Url(url ...string) string {
 	if len(url) > 0 {
 		c.url = url[0]
 	}
 	return c.url
 }
 
-func (c Client) Secret(sec ...string) string {
+func (c *Client) Secret(sec ...string) string {
 	if len(sec) > 0 {
 		c.secret = sec[0]
 	}
 	return c.secret
 }
 
-func (c Client) UA(ua ...string) string {
+func (c *Client) UA(ua ...string) string {
 	if len(ua) > 0 {
 		c.userAgent = ua[0]
 	}
 	return c.userAgent
 }
 
-func (c Client) Version(v ...string) string {
+func (c *Client) Version(v ...string) string {
 	if len(v) > 0 {
 		c.apiVersion = v[0]
 	}
@@ -59,8 +59,9 @@ func (c Client) HttpResponse() *http.Response {
 func (c Client) Call(method string, path string, data io.Reader) (*http.Response, error) {
 	var err error
 	var request *http.Request
-	var url string = strings.Join([]string{c.url, path}, "/")
+	var url string
 
+	url = strings.Join([]string{c.url, path}, "/")
 	request, err = http.NewRequest(method, url, data)
 	if err != nil {
 		return nil, err
